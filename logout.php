8c43a41ee3d24239f2801406190fe8a9510c1d9e
ArditJakaj <aj70566@ubt-uni.net>
2025-02-02 23:09:54 +0100

    permisimet e fundit

--- logout.php
+++ logout.php
@@ -1,6 +1,20 @@
 <?php
 session_start();
+session_unset();
 session_destroy();
-header("Location: login.html");
+
+setcookie("username", "", time() - 3600, "/");
+setcookie("password", "", time() - 3600, "/");
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+
+
+header("Location: login.php");
 exit();
+
+
+
 ?>
